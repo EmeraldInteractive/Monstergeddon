0a620b20486424ca5ad66c2478bc185faeeb0d10
Emerald Interactive <interactiveemerald@gmail.com>
2017-10-01 12:57:47 -0500

    Tutorial turns on and off, manual cursor also works

---
+++ Assets/Scripts/ManualCursorLock.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ManualCursorLock : MonoBehaviour
+{
+
+    void Update()
+    {
+        if (Input.GetButton("Fire1"))
+        {
+            CursorLock();
+        }
+
+        if (Input.GetKeyUp(KeyCode.Escape))
+        {
+            CursorUnlock();
+        }
+    }
+
+    void CursorLock()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    void CursorUnlock()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+}
